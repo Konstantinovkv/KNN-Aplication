4e1e05b51c41f9b07d15ee0371ac1000f09d6d69
Konstantin Konstantinov <konstantinovkv@gmail.com>
2023-04-01 20:05:54 +0300

    Add default cassandra authentication in a properties file

--- src/main/java/knn/dbUtils/CassandraConnector.java
+++ src/main/java/knn/dbUtils/CassandraConnector.java
@@ -1,12 +1,38 @@
 package knn.dbUtils;
 
 import com.datastax.oss.driver.api.core.CqlSession;
-import com.datastax.oss.driver.api.core.CqlSessionBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.util.Properties;
 
 public class CassandraConnector {
+
     public static CqlSession connect() {
-        CqlSessionBuilder builder = CqlSession.builder()
-                .withKeyspace("knn");
-        return builder.build();
+        Properties properties = loadProperties();
+
+        String ipAddress = properties.getProperty("cassandra.ip_address");
+        String username = properties.getProperty("cassandra.username");
+        String password = properties.getProperty("cassandra.password");
+        String localDatacenter = properties.getProperty("cassandra.local_datacenter");
+        String keyspace = properties.getProperty("cassandra.keyspace");
+
+        return CqlSession.builder()
+                .addContactPoint(new InetSocketAddress(ipAddress, 9042))
+                .withAuthCredentials(username, password)
+                .withKeyspace(keyspace)
+                .withLocalDatacenter(localDatacenter)
+                .build();
+    }
+
+    private static Properties loadProperties() {
+        Properties properties = new Properties();
+        try (InputStream inputStream = CassandraConnector.class.getClassLoader().getResourceAsStream("application.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            System.err.println("Error loading application.properties: " + e.getMessage());
+        }
+        return properties;
     }
 }
