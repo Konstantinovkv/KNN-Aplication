f2115a40233a8ce4745a5d25670820c7d65b3362
Konstantin Konstantinov <konstantinovkv@gmail.com>
2023-04-01 15:22:52 +0300

    change python script

--- dbFiller.py
+++ dbFiller.py
@@ -2,6 +2,7 @@ from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 import uuid
 import random
+import logging
 
 def generate_data_points(n):
     labels = ['A', 'B', 'C']
@@ -17,7 +18,7 @@ def generate_data_points(n):
 
 def insert_data_points(session, data_points):
     insert_query = '''
-        INSERT INTO my_keyspace.data_points (id, feature_1, feature_2, label)
+        INSERT INTO knn.data_points (id, feature_1, feature_2, label)
         VALUES (%s, %s, %s, %s);
     '''
 
@@ -25,15 +26,24 @@ def insert_data_points(session, data_points):
         feature1, feature2, label = data_point
         session.execute(insert_query, (uuid.uuid4(), feature1, feature2, label))
 
+logging.basicConfig(level=logging.INFO)
+
 def main():
-    auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
-    cluster = Cluster(['127.0.0.1'], auth_provider=auth_provider)
-    session = cluster.connect()
+    try:
+        auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
+        cluster = Cluster(['127.0.0.1'], auth_provider=auth_provider)
+        session = cluster.connect()
+    except Exception as e:
+        logging.error(f"Error connecting to Cassandra: {e}")
+        return
 
-    data_points = generate_data_points(100)
-    insert_data_points(session, data_points)
-
-    print("Inserted test data into the data_points table.")
+    try:
+        data_points = generate_data_points(100)
+        insert_data_points(session, data_points)
+    except Exception as e:
+        logging.error(f"Error inserting data points: {e}")
+    else:
+        logging.info("Inserted test data into the data_points table.")
 
     session.shutdown()
     cluster.shutdown()
